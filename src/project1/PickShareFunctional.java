9a1fb0c506db0c2f30e860b522571ecbd7d021a2
Phlana <joshua.chang422@gmail.com>
2020-01-26 14:16:06 -0700

    finished

--- src/project1/PickShareFunctional.java
+++ src/project1/PickShareFunctional.java
@@ -1,8 +1,31 @@
 package project1;
 
-public class PickShareFunctional {
-    public ShareInfo findHighPriced() {
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
+public class PickShareFunctional {
+    final static int highestPrice = 500;
+    public static ShareInfo findHighPrices(Stream<String> stream) {
+        return stream
+                .map(ShareUtil::getPrice)
+                .filter(shareInfo -> ShareUtil.isPriceLessThan(highestPrice).test(shareInfo))
+                .reduce(ShareUtil::pickHigh)
+                .get();
+//                .filter(shareInfo -> ((ShareInfo) shareInfo).price.compareTo(highestPrice) < 0)
+//                .max(Comparator.comparing(s -> s.price)).get();
     }
 
+    public static void main(String[] args) {
+        // 1637 ms
+        long startTime = System.currentTimeMillis();
+        System.out.println("highest under 500: " + findHighPrices(Shares.symbols.stream()));
+        long timeElapsed = System.currentTimeMillis() - startTime;
+        System.out.println("time elapsed: " + timeElapsed);
+
+//        // 1207 ms
+//        long startTime = System.currentTimeMillis();
+//        System.out.println("highest under 500: " + findHighPrices(Shares.symbols.parallelStream()));
+//        long timeElapsed = System.currentTimeMillis() - startTime;
+//        System.out.println("time elapsed: " + timeElapsed);
+    }
 }
